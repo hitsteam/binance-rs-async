ffd7676d323f692e49cd3e34f39afdea85cfb3fc
Ethan Y. Tsai <yee.tsai@gmail.com>
2023-12-28 12:26:05 +0800

    update tran id and trade id type

--- src/futures/rest_model.rs
+++ src/futures/rest_model.rs
@@ -677,6 +677,6 @@ pub struct AccountIncome {
     pub asset: String,
     pub info: String,
     pub time: u64,
-    pub tran_id: String,
-    pub trade_id: String,
+    pub tran_id: u64,
+    pub trade_id: u64,
 }
